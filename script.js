faa97a62d84e673ddb96886a9d564da674988027
brayan3229 <brayaniquira@unimayor.edu.co>
2024-09-17 17:19:36 -0500

    sisi

---
+++ script.js
@@ -0,0 +1,6 @@
+// Seleccionar el elemento <h1> y agregarle un evento 'click'
+document.querySelector('h1').addEventListener('click', () => {
+    console.log('El título ha sido clicado');
+    // Puedes añadir aquí cualquier acción que desees ejecutar al hacer clic en <h1>
+});
+
